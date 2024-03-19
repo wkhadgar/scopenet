d0ecb962c0f640f3c19a62a7c8d71eb1e09287bb
Paulo Santos <pauloroberto.santos@edge.ufal.br>
2024-03-19 19:22:00 -0300

    feat: Habilita multi-thread.

--- scope-client.py
+++ scope-client.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import threading
 import tkinter as tk
 from tkinter import ttk
 
@@ -7,57 +8,64 @@ from tkinter import ttk
 HOST = '127.0.0.1'  # Endereço IP do servidor
 PORT = 65432  # Porta do servidor
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
 
-def send_command(command: dict) -> str:
-    """
-    Envia comandos ao servidor (telescópio na rede)
+command_requests = {
+    "DISCONNECT": False,
+    "GOTO": False,
+    "TRACK": False,
+}
 
-    :param command: dicionário de instruções em formato de json
-    """
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((HOST, PORT))
-            s.sendall(json.dumps(command).encode())
-            response = s.recv(1024)
-            return "Successfully sent command\n" + ("-" * 70) + "\n" + f"Response: {response.decode()}"
-    except ConnectionRefusedError:
-        return "Unable to connect with telescope.\n" + ("-" * 70) + "\n"
-
-
-# Exemplo de comandos
+commands = {
+    "DISCONNECT": {
+        "command": "DISCONNECT",
+        "metadata": "NULL"
+    },
+    "GOTO": {
+        "command": "GOTO",
+        "metadata": {
+            "ra": "",
+            "dec": "",
+        }
+    },
+    "TRACK": {
+        "command": "TRACK",
+        "metadata": "NULL"
+    },
+}
 
 
 def on_track():
-    response = send_command({
-        "command": "TRACK",
-        "metadata": "NULL"
-    })
-
-    response_label.config(text=response)
+    command_requests["TRACK"] = True
 
 
 def on_goto():
-    ra_value = f"{ra_hour.get()}h {ra_minute.get()}m {ra_second.get()}s"
-    dec_value = f"{dec_degree.get()}° {dec_minute.get()}' {dec_second.get()}\""
-    command = {
-        "command": "GOTO",
-        "metadata": {
-            "ra": ra_value,
-            "dec": dec_value
-        }
-    }
-    response = send_command(command)
-
-    response_label.config(text=response)
+    commands["GOTO"]["metadata"]["ra"] = f"{ra_hour.get()}h {ra_minute.get()}m {ra_second.get()}s"
+    commands["GOTO"]["metadata"]["dec"] = f"{dec_degree.get()}° {dec_minute.get()}' {dec_second.get()}\""
+    command_requests["GOTO"] = True
 
 
 def on_dc():
-    response = send_command({
-        "command": "DISCONNECT",
-        "metadata": "NULL"
-    })
+    command_requests["DISCONNECT"] = True
 
+
+def message_manager():
+    response = ("-" * 70) + "\n" + f"{s.recv(1024).decode()}"
     response_label.config(text=response)
+    run = True
+    while run:
+        for cbr in list(command_requests.keys()):
+            if command_requests[cbr]:
+                try:
+                    s.send(json.dumps(commands[cbr]).encode())
+                    response = "Successfully sent command\n" + ("-" * 70) + "\n" + f"Response: {s.recv(1024).decode()}"
+                except:
+                    response = "Unable to connect with telescope.\n" + ("-" * 70) + "\n"
+                    s.close()
+                    run = False
+                command_requests[cbr] = False
+                response_label.config(text=response)
 
 
 if __name__ == "__main__":
@@ -67,65 +75,55 @@ if __name__ == "__main__":
     root.title("Cliente ScopeNet")
     root.iconbitmap("assets/favicon.ico")
 
-    btn_track = ttk.Button(root, text="Track", command=on_track)
-    btn_track.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
-
     separator = ttk.Separator(root, orient='horizontal')
-    separator.grid(row=1, column=0, columnspan=2, sticky='ew', padx=10, pady=5)
+    separator.grid(row=0, column=0, columnspan=2, sticky='ew', padx=10, pady=5)
 
-    frame_goto = ttk.Frame(root)
-    frame_goto.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
-
-    btn_goto = ttk.Button(frame_goto, text="GoTo", command=on_goto)
-    btn_goto.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
-
-    frame_ra = ttk.Frame(frame_goto)
-    frame_ra.grid(row=0, column=0, padx=10, pady=5)
-
+    frame_ra = ttk.Frame(root)
+    frame_ra.grid(row=1, column=0, padx=10, pady=5)
     ra_hour = tk.StringVar()
-    ra_minute = tk.StringVar()
-    ra_second = tk.StringVar()
-
     ra_hour_entry = ttk.Entry(frame_ra, width=5, textvariable=ra_hour)
     ra_hour_entry.grid(row=0, column=0, padx=(0, 5))
-
+    ttk.Label(frame_ra, text="h").grid(row=0, column=1, padx=(0, 5))
+    ra_minute = tk.StringVar()
     ra_minute_entry = ttk.Entry(frame_ra, width=5, textvariable=ra_minute)
     ra_minute_entry.grid(row=0, column=2, padx=(0, 5))
-
+    ttk.Label(frame_ra, text="m").grid(row=0, column=3, padx=(0, 5))
+    ra_second = tk.StringVar()
     ra_second_entry = ttk.Entry(frame_ra, width=5, textvariable=ra_second)
     ra_second_entry.grid(row=0, column=4, padx=(0, 5))
-
-    ttk.Label(frame_ra, text="h").grid(row=0, column=1, padx=(0, 5))
-    ttk.Label(frame_ra, text="m").grid(row=0, column=3, padx=(0, 5))
     ttk.Label(frame_ra, text="s").grid(row=0, column=5)
 
-    frame_dec = ttk.Frame(frame_goto)
-    frame_dec.grid(row=0, column=1, padx=10, pady=5)
-
+    frame_dec = ttk.Frame(root)
+    frame_dec.grid(row=1, column=1, padx=10, pady=5)
     dec_degree = tk.StringVar()
-    dec_minute = tk.StringVar()
-    dec_second = tk.StringVar()
-
     dec_degree_entry = ttk.Entry(frame_dec, width=5, textvariable=dec_degree)
     dec_degree_entry.grid(row=0, column=0, padx=(0, 5))
-
+    ttk.Label(frame_dec, text="°").grid(row=0, column=1, padx=(0, 5))
+    dec_minute = tk.StringVar()
     dec_minute_entry = ttk.Entry(frame_dec, width=5, textvariable=dec_minute)
     dec_minute_entry.grid(row=0, column=2, padx=(0, 5))
-
+    ttk.Label(frame_dec, text="'").grid(row=0, column=3, padx=(0, 5))
+    dec_second = tk.StringVar()
     dec_second_entry = ttk.Entry(frame_dec, width=5, textvariable=dec_second)
     dec_second_entry.grid(row=0, column=4, padx=(0, 5))
-
-    ttk.Label(frame_dec, text="°").grid(row=0, column=1, padx=(0, 5))
-    ttk.Label(frame_dec, text="'").grid(row=0, column=3, padx=(0, 5))
     ttk.Label(frame_dec, text="\"").grid(row=0, column=5)
+
+    btn_goto = ttk.Button(root, text="GoTo", command=on_goto)
+    btn_goto.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
 
     separator = ttk.Separator(root, orient='horizontal')
     separator.grid(row=3, column=0, columnspan=2, sticky='ew', padx=10)
 
+    btn_track = ttk.Button(root, text="Track", command=on_track)
+    btn_track.grid(row=4, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
+
     response_label = tk.Label(root, text="-" * 70 + "\n", font=('Arial', 12), wraplength=400)
-    response_label.grid(row=4, column=0, columnspan=2, padx=10, pady=(10, 0))
+    response_label.grid(row=5, column=0, columnspan=2, padx=10, pady=(10, 0))
 
     btn_finish = tk.Button(root, text="Finish Connection", command=on_dc)
-    btn_finish.grid(row=5, column=0, columnspan=2, padx=10, pady=(10, 0))
+    btn_finish.grid(row=6, column=0, columnspan=2, padx=10, pady=(10, 0))
+
+    thread = threading.Thread(target=message_manager)
+    thread.start()
 
     root.mainloop()
--- scope-server.py
+++ scope-server.py
@@ -1,23 +1,27 @@
 import socket
 import json
+import threading
 
 target_ra = {"h": 0, "m": 0, "s": 0}
 target_dec = {"°": 0, "'": 0, "\"": 0}
+
+slewing = False
 
 
 def save_target_loc(goto_metadata: dict[str, str]):
     ra_recv = goto_metadata['ra'].split(" ")
     dec_recv = goto_metadata['dec'].split(" ")
-    try:
-        target_ra[ra_recv[0][-1]] = int(ra_recv[0][:-1])
-        target_ra[ra_recv[1][-1]] = int(ra_recv[1][:-1])
-        target_ra[ra_recv[2][-1]] = int(ra_recv[2][:-1])
 
-        target_dec[dec_recv[0][-1]] = int(dec_recv[0][:-1])
-        target_dec[dec_recv[1][-1]] = int(dec_recv[1][:-1])
-        target_dec[dec_recv[2][-1]] = int(dec_recv[2][:-1])
-    except:
+    try:
+        for i, k in enumerate(target_ra.keys()):
+            target_ra[k] = int(ra_recv[i][:-1])
+
+        for i, k in enumerate(target_dec.keys()):
+            target_dec[k] = int(dec_recv[i][:-1])
+
+    except ValueError:
         return -1
+
     return 0
 
 
@@ -28,12 +32,12 @@ def process_command(command):
     :param command: Comando recebido como dicionário json.
     :return: Resposta ao comando.
     """
-
+    if slewing:
+        return "Telescope is slewing. Wait to finish."
     if command["command"] == "TRACK":
         return ("Tracking current position:\n"
                 f"RA: {target_ra};\nDEC: {target_dec}")
     elif command["command"] == "GOTO":
-
         if save_target_loc(command['metadata']) != 0:
             return f"Incorrect data:\nRA={command['metadata']['ra']}, DEC={command['metadata']['dec']}"
         else:
@@ -44,32 +48,52 @@ def process_command(command):
         return f"Command '{command['command']}' not recognized..."
 
 
+def handle_connection(client: socket):
+    connected = True
+    while connected:
+        try:
+            # Recebe os dados do cliente
+            data = client.recv(1024).decode()
+
+            if data:
+                # Decodifica os dados recebidos do cliente
+                command_json = json.loads(data)
+                print(f"[{addr[0]}:{addr[1]}]: {command_json['command']}")
+                response = process_command(command_json)
+
+                if response == "DISCONNECT":
+                    connected = False
+                    response = "Telescope detached"
+                    connected_clients.remove(client)
+                    print("Fim de conexão solicitado.")
+
+                client.send(response.encode())
+        except:
+            connected_clients.remove(client)
+            connected = False
+            print("Conexão perdida.")
+    client.close()
+
+
 if __name__ == "__main__":
     HOST = '127.0.0.1'  # Endereço IP local
     PORT = 65432
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        print("Telescope is waiting for commands...")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen()
 
-        connected = True
-        while connected:
-            conn, addr = s.accept()
-            with conn:
-                while True:
-                    # Recebe os dados do cliente
-                    data = conn.recv(1024)
-                    if not data:
-                        break
+    connected_clients = []
+    print("Telescope is waiting for connection...")
 
-                    # Decodifica os dados recebidos do cliente
-                    command_json = json.loads(data.decode())
-                    print(f"[{addr[0]}:{addr[1]}]: {command_json['command']}")
-                    response = process_command(command_json)
-                    if response == "DISCONNECT":
-                        connected = False
-                        response = "Telescope turned off."
-                        print("Fim de conexão solicitado.")
+    while True:
+        conn, addr = s.accept()
+        if conn not in connected_clients:
+            print(f"Connected to {addr[0]}! Waiting for commands...")
+            connected_clients.append(conn)
+            conn.send("Connected successfully.".encode())
 
-                    conn.sendall(response.encode())
+            thread = threading.Thread(target=handle_connection, args=(conn,))
+            thread.start()
+        else:
+            conn.send("Already connected.".encode())
